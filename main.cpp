957c32ef5ddd36d5ce43a6d1ae768eebe96ed8a2
JeyJer <JeyJer@hotmail.fr>
2014-07-23 02:48:29 +0200

    Main menu made
    Client can choose to make a new game or to exit application.

--- main.cpp
+++ main.cpp
@@ -1,9 +1,39 @@
 #include <iostream>
 
-using namespace std;
+#include "OverWorld.h"
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    std::cout << "\t\tBienvenue sur le jeu de ouf OverWorld Adventure\n" << std::endl;
+
+    int choice;
+    do{
+        do
+        {
+            std::cout << "1. Nouvelle partie\n2. Quitter\n\n";
+            std::cin >> choice;
+
+            if( choice != 1 && choice != 2 )
+                std::cout << "Veuillez choisir l'option 1 ou 2, fils de pute.";
+
+        } while( choice != 1 && choice != 2 );
+
+        switch(choice)
+        {
+            case 1 :
+            {
+                OverWorld* ow = new OverWorld;
+                ow->run();
+                break;
+            }
+            case 2 :
+                std::cout << "Merci d'avoir joué sur OverWorld Adventure!!\n";
+                return 0;
+            default :
+                std::cout << "Choix inconnu...\n";
+                return 0;
+        }
+    } while( true );
+
     return 0;
 }
